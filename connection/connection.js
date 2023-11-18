794a5632ac22ade0b812d3b4d0d2c9e3b05d1431
lucasvicente98 <lucasleitev98@gmail.com>
2023-11-18 18:02:37 -0300

    configurado o arquivo de conexao ao mysql

--- connection/connection.js
+++ connection/connection.js
@@ -1,1 +1,12 @@
 
+// conexão
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hackfaesa23',
+    database: 'emissao_co2',
+});
+
+module.exports = connection;
